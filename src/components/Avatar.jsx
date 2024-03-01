d44a559f33091f858fe065b60c0171227d9ccb67
GabrielFerreiraTSI <117236024+GabrielFerreiraTSI@users.noreply.github.com>
2024-03-01 17:08:03 -0300

    initial commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Avatar(props) {
+    return <img className="circle-image" src={props.img} alt="profile_image" />
+}
+
+export default Avatar;
---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+    {
+        id: 1,
+        name: "Contact 1",
+        imgURL: "https://media-gig4-1.cdn.whatsapp.net/v/t61.24694-24/424454230_1051868815927533_8711795099995780090_n.jpg?ccb=11-4&oh=01_AdTnm43lYqGDLB1HoK5kTwjpMD7iG3n7IJ44b39aMwT-yQ&oe=65EF3089&_nc_sid=e6ed6c&_nc_cat=111",
+        tel: "+55 (XX)XXXXX-XXXX"
+    },
+
+    {
+        id: 2,
+        name: "Contact 2",
+        imgURL: "https://media-gig4-1.cdn.whatsapp.net/v/t61.24694-24/368136285_293127216784584_6205056533106934126_n.jpg?ccb=11-4&oh=01_AdS2-TpR3tHiPkj0JyTd-Ozs1OIu2k1wf3-A-rPkqw7AAA&oe=65EF4968&_nc_sid=e6ed6c&_nc_cat=107",
+        tel: "+55 (XX)XXXXX-XXXX"
+    },
+
+    {
+        id: 3,
+        name: "Contact 3",
+        imgURL: "https://media-gig4-1.cdn.whatsapp.net/v/t61.24694-24/95076184_613741942818075_5677636024161585904_n.jpg?ccb=11-4&oh=01_AdRj61tPrwpkapSakTqpw2gY1vzSN-sR7atb8Wk1FNHogQ&oe=65EF4312&_nc_sid=e6ed6c&_nc_cat=106",
+        tel: "+55 (XX)XXXXX-XXXX"
+    }
+];
+
+export default contacts;
